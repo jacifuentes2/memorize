b41264f74b379dffc5a25c7b8f12cac7dfececad
kleer <kleer@kleer-csd-box>
2017-06-14 09:04:22 -0500

    Inicio Feature

---
+++ features/memorize.feature
@@ -0,0 +1,6 @@
+Feature: Memorize
+
+Scenario: Jugar Memorize
+	Given Visitar pagina
+	When Selecciona Boton "Imagen 1"
+	Then Debe mostrar el valor "7"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,12 @@
+Given(/^Visitar pagina$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^Selecciona Boton "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Debe mostrar el valor "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
